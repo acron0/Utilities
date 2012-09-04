7ed3d3adde899382302ed5a6769a1deef1acf82c
Antony Woods <acron1@gmail.com>
2012-09-04 17:02:33 +0100

    deadendthrills.js progress :)

--- deadendthrills/deadendthrills.js
+++ deadendthrills/deadendthrills.js
@@ -7,34 +7,51 @@ var _entryPoint;
 // - the minimum delay between a transition (it will surpass this if the load is taking longer)
 var _minDelay;
 
-// - the two images used for displaying and transitioning.
-var _imageA, _imageB;
+// - the time it takes to fade between images
+var _fadeOverTime;
 
-// - 
+// - the image array
+var _images = [];
+
+// - current index
+var _currentImageIndex;
+
+// - the preloader, if one exists
 var _preloader;
+
+// - boolean, true if loading
+var _loading = false;
 
 // - the jsonp callback url
 //var _imageCallbackUrl = 'http://localhost:8000/deadendthrills';
 //var _imageCallbackUrl = 'http://teamwoods.org/deadendthrills/jsonp_callback=?';
 var _imageCallbackUrl = 'http://localhost:8000/jsonp_callback=?';
 
+// - CONSTS
+var NOOF_IMAGES = 2;
+
 
 // functions
 // -------------------------------------
 
 // - gets a new image url from offsite
-function getNewImage(startMinDelay) 
+function getNewImage(startMinDelay, imgIndex) 
 {	
+	_currentImageIndex = imgIndex;
+
 	// get image url from jsonp	
 	var jqxhr = $.getJSON(_imageCallbackUrl, function(json){data = json});
 	jqxhr.success(successFunction);
 	jqxhr.error(errorFunction);
+	
+	_loading = true;
 }
 
 // - the jsonp callback returned successfully
 function successFunction()
 {
-	$(_imageA).attr("src",data.url);	
+	console.log("[deadendthrills] Loading " + data.url);
+	$(_images[_currentImageIndex]).attr("src", data.url);	
 }
 
 // - the jsonp callback returned an error
@@ -43,45 +60,76 @@ function errorFunction(XMLHttpRequest,textStatus, errorThrown)
 	console.log("An error occurred.");
 }
 
+// - creates a new img tag with the correct style
 function createImage()
 {
 	var newImage =  document.createElement('img');
 	newImage.onload = imageLoadedCallback;
-	$(newImage).attr("style","width:100%;display:none;");
+	newImage.onerror = imageErrorCallback;
+	$(newImage).attr("style","width:100%;display:none;position:absolute;");
 	return newImage;
 }
 
+// - call back once the image has loaded
 function imageLoadedCallback(object)
 {	
+	_loading = false;
+	
 	if(_preloader != null)
 	{
 		$(_preloader).remove();
 	}
-		
-	console.log("image loaded");
 	
-	$(_imageA).css("display","block");	
+	$(_images[_currentImageIndex]).css({"display":"block", "opacity":"0"});
+	
+	for(var i = 0; i < NOOF_IMAGES; ++i)
+	{	
+		var index = i;
+		$(_images[i]).animate(
+		{
+			opacity: i == _currentImageIndex ? 1 : 0,
+		}, _fadeOverTime, function() 
+			{
+				if(this == _images[_currentImageIndex] && !_loading)
+				{
+					// anim complete - fetch the next image
+					var nextImageIndex = _currentImageIndex + 1;
+					if(nextImageIndex >= NOOF_IMAGES)
+						nextImageIndex = 0;
+					getNewImage(true, nextImageIndex);
+				}
+			}
+		);
+	}
+}
+
+// - callback if the image load errors
+function imageErrorCallback(object)
+{	
+	// try again
+	getNewImage(true, _currentImageIndex);
 }
 
 // - [jQuery] the DOM is ready 
 $(document).ready(function() 
 {
 	_entryPoint = document.getElementById("deadendthrills_slideshow");
-	_minDelay = _entryPoint.getAttribute("delay");
+	_minDelay = parseInt(_entryPoint.getAttribute("delay"), 10);
+	_fadeOverTime = parseInt(_entryPoint.getAttribute("fadeover"), 10);
 	
 	_preloader = document.getElementById("deadendthrills_preloader");
-	console.log(_preloader);
 	
 	var innerDiv = document.createElement('div');
-	$(innerDiv).attr("style","width:100%;margin: 0px auto;");
+	$(innerDiv).attr("style","width:100%;margin:0px auto;");
 
-	_imageA = createImage();
-	_imageB = createImage();
+	for(var i = 0; i < NOOF_IMAGES; ++i)
+	{
+		_images[i] = createImage();
+		innerDiv.appendChild(_images[i]);
+	}
 	
-	innerDiv.appendChild(_imageA);
-	innerDiv.appendChild(_imageB);
 	_entryPoint.appendChild(innerDiv);
 	
-	getNewImage(false);
+	getNewImage(false, 0);
 
 });
